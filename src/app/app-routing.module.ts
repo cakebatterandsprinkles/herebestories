b5b7d0f5bdfda5394ed4e17672f4fe4cc86fe469
yagmurcetintas <yagmurcetin@gmail.com>
2019-11-15 10:33:50 -0500

    added routing to navbar and made the initial write, settings, profile, read pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import {LandingComponent} from './components/pages/landing/landing.component';
 import {SignupComponent} from './components/pages/signup/signup.component';
 import {LoginComponent} from './components/pages/login/login.component';
 import {DashboardComponent} from './components/pages/dashboard/dashboard.component';
+import {WriteComponent} from './components/pages/write/write.component';
+import {ProfileComponent} from './components/pages/profile/profile.component';
+import {SettingsComponent} from './components/pages/settings/settings.component';
+import {ReadComponent} from './components/pages/read/read.component';
 
 const routes : Routes = [
     {
@@ -22,6 +26,18 @@ const routes : Routes = [
     }, {
         path: 'dashboard',
         component: DashboardComponent
+    }, {
+        path: 'write',
+        component: WriteComponent
+    }, {
+        path: 'profile',
+        component: ProfileComponent
+    }, {
+        path: 'settings',
+        component: SettingsComponent
+    }, {
+        path: 'read',
+        component: ReadComponent
     }
 ];
 
