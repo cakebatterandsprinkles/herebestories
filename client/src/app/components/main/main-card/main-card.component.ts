b918ae4407d5c0ac51ae779254195359a88b0105
yagmurcetintas <yagmurcetin@gmail.com>
2019-11-17 16:25:33 -0500

    added like button to the dashboard cards

--- client/src/app/components/main/main-card/main-card.component.ts
+++ client/src/app/components/main/main-card/main-card.component.ts
@@ -6,15 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-card.component.scss']
 })
 export class MainCardComponent implements OnInit {
-
   chosenImage: string;
   chosenUrl: string;
+  chosenLikeImg: string;
+  likeCount = 0;
+
+
+  onClick() {
+    if (this.chosenLikeImg === '/assets/images/heart-empty.png') {
+      this.chosenLikeImg = '/assets/images/heart-full.png';
+      this.likeCount++;
+    } else if (this.chosenLikeImg === '/assets/images/heart-full.png') {
+      this.chosenLikeImg = '/assets/images/heart-empty.png';
+      this.likeCount--;
+    }
+  }
 
   ngOnInit() {
-    const images: any[] = ['card1', 'card2', 'card3', 'card4', 'card5', 'card6', 'card7', 'card8', 'card9'];
+    const images: any[] = [
+      'card1',
+      'card2',
+      'card3',
+      'card4',
+      'card5',
+      'card6',
+      'card7',
+      'card8',
+      'card9'
+    ];
 
     const num = Math.floor(Math.random() * images.length);
     this.chosenImage = images[num];
     this.chosenUrl = `/assets/images/${this.chosenImage}.png`;
+    this.chosenLikeImg = `/assets/images/heart-empty.png`;
   }
 }
