2088af9d2cf1c3f5aaf75701f6cc74dd45fb7cbe
yagmurcetintas <yagmurcetin@gmail.com>
2019-11-16 19:22:54 -0500

    Added prompt page

---
+++ src/app/components/main/main-writing-content/main-writing-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-writing-content',
+  templateUrl: './main-writing-content.component.html',
+  styleUrls: ['./main-writing-content.component.scss']
+})
+export class MainWritingContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
